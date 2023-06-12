b7827adca74984a31912d07c62514425149c2e24
harim choi <apologus0395@gmail.com>
2023-06-12 11:40:08 +0900

    디자인 리뉴얼

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import OhbleLogo from "./images/ohble_logo.png";
 
 export default function Home() {
   // form 데이터 자료형 지정
@@ -12,8 +13,6 @@ export default function Home() {
 
   // 물음표 버튼 State 설정
   const [visible, setVisible] = useState(false);
-  // Submit 버튼 State 설정
-  // const [submit, setSubmit] = useState(false);
 
   // handleSumbit : 제출시 이벤트 제어
   // watch : 입력값를 감시하는 기능
@@ -45,118 +44,118 @@ export default function Home() {
     });
   };
 
-  // const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
-  //   // 제출해도 페이지 새로고침 안되도록 하는 함수.
-  //   event.preventDefault();
-  // };
   return (
-    <form
-      acceptCharset="utf-8"
-      method="get"
-      className="box"
-      onSubmit={handleSubmit(onValid)}
-    >
-      <h1>오블 인플루언서 수요조사</h1>
-      <p>블로그의 ID를 입력하세요</p>
-      <a
-        className="questionMark"
-        // 물음표 Hover 시 id_description 나타나기
-        onMouseEnter={() => {
-          setVisible(true);
-        }}
-        onMouseLeave={() => {
-          setVisible(false);
-        }}
+    <>
+      <img className="logo" src={OhbleLogo.src} alt="ohbleLogo" />
+      <div className="line"></div>
+      <form
+        acceptCharset="utf-8"
+        method="get"
+        className="box"
+        onSubmit={handleSubmit(onValid)}
       >
-        ?
-      </a>
-      {visible && (
-        <div className="id_description">
-          <p className="id_example1">ex) https://blog.naver.com/ </p>
-          <p className="id_example2">ohble</p>
+        <h1>오블 인플루언서 수요조사</h1>
+
+        <div className="inputSection">
+          <div className="settings">
+            <label style={{}} htmlFor="id">
+              <a
+                className="questionMark"
+                // 물음표 Hover 시 id_description 나타나기
+                onMouseEnter={() => {
+                  setVisible(true);
+                }}
+                onMouseLeave={() => {
+                  setVisible(false);
+                }}
+              >
+                ?
+              </a>
+              {visible && (
+                <div className="id_description">
+                  자신의 블로그 주소창의 마지막 부분을 입력해주세요
+                  <div className="wrapper">
+                    <p className="id_example2">ex) https://blog.naver.com/ </p>
+                    <p className="id_example3">ohble</p>
+                  </div>
+                </div>
+              )}
+              블로그 ID
+            </label>
+            <input
+              aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
+              {...register("id", {
+                required: "블로그 아이디를 입력해주세요",
+                pattern: {
+                  value: /^[A-Za-z0-9]+$/i,
+                  message: "아이디는 소문자, 대문자, 숫자로 구성되어야합니다",
+                },
+              })}
+              type="text"
+              className="id"
+              placeholder="블로그 ID를 입력하세요"
+            />
+          </div>
+          {errors.id && <small role="alert">{errors.id.message}</small>}
+          <div className="settings">
+            <label htmlFor="email">이메일</label>
+
+            <input
+              {...register("email", {
+                required: "이메일을 입력하세요.",
+                pattern: {
+                  value:
+                    /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/,
+                  message: "알맞은 이메일이 아닙니다.",
+                },
+              })}
+              type="text"
+              className="email"
+              placeholder="이메일을 입력하세요"
+            />
+          </div>
+          {errors.email && <small role="alert">{errors.email.message}</small>}
+          <div className="dashed_line"></div>
+          <p>* 나이와 성별은 선택사항입니다</p>
+          <div className="settings">
+            <label htmlFor="age">나이</label>
+            <input
+              {...register("age")}
+              type="number"
+              placeholder="나이를 입력해주세요"
+            />
+          </div>
+          {errors.age && <small role="alert">{errors.age.message}</small>}
+          <div className="settings">
+            <label htmlFor="gender" style={{ marginRight: "30px" }}>
+              성별
+            </label>
+            <div className="radio_box">
+              <input
+                {...register("gender")}
+                type="radio"
+                id="gender1"
+                value="male"
+              />
+              <label htmlFor="gender1">남자</label>
+
+              <input
+                {...register("gender")}
+                type="radio"
+                id="gender2"
+                value="female"
+              />
+              <label htmlFor="gender2">여자</label>
+            </div>
+          </div>
         </div>
-      )}
-      <input
-        aria-invalid={!isDirty ? undefined : errors.id ? "true" : "false"}
-        {...register("id", {
-          required: "블로그 아이디를 입력해주세요",
-          pattern: {
-            value: /^[A-Za-z0-9]+$/i,
-            message: "아이디는 소문자, 대문자, 숫자로 구성되어야합니다",
-          },
-        })}
-        type="text"
-        className="id"
-        placeholder="블로그 ID를 입력하세요"
-      />
-      {errors.id && <small role="alert">{errors.id.message}</small>}
-      <p>이메일을 입력하세요</p>
-      <input
-        {...register("email", {
-          required: "이메일을 입력하세요.",
-          pattern: {
-            value:
-              /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/,
-            message: "알맞은 이메일이 아닙니다.",
-          },
-        })}
-        type="text"
-        className="email"
-        placeholder="이메일을 입력하세요"
-      />
-      <p>* 나이와 성별은 선택사항입니다</p>
-      <br />
-      {errors.email && <small role="alert">{errors.email.message}</small>}
-      <p>나이를 입력해주세요</p>
-      <input
-        {
-          ...register("age")
-          // , {
-          //   required: "나이를 입력하세요",
-          //   pattern: {
-          //     value: /^[0-9]{1,2}$/,
-          //     message: "알맞은 값이 아닙니다. 다시입력해주세요",
-          //   },
-          // }
-        }
-        type="number"
-        placeholder="나이를 입력해주세요"
-      />
-      {errors.age && <small role="alert">{errors.age.message}</small>}
-      <p style={{ display: "block" }}>성별을 입력해주세요</p>
-      <input
-        {...register("gender")}
-        type="radio"
-        className="gender"
-        value="male"
-        name="gender"
-      />
-      <p>남성</p>
-      <input
-        {...register("gender")}
-        type="radio"
-        className="gender"
-        value="female"
-        name="gender"
-      />
-      <p>여성</p>
-      <input
-        type="submit"
-        className="submit"
-        value="제출하기"
-        disabled={isSubmitting}
-      />
-      {/* {submit && (
-        <div className="endingBox">
-          <div
-            className="closeBtn"
-            onClick={() => {
-              setSubmit(false);
-            }}
-          ></div>
-          <h2> 정상적으로 제출되었습니다</h2>
-        </div>
-      )} */}
-    </form>
+        <input
+          type="submit"
+          className="submit"
+          value="제출하기"
+          disabled={isSubmitting}
+        />
+      </form>
+    </>
   );
 }
