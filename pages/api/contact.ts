09d24ff55b43028be19becdae0337a1b4bddbaf7
조기범 <akfmzh1207@naver.com>
2023-06-26 17:43:35 +0900

    css 맞추기

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -16,7 +16,7 @@ export default function sendGmail(req: NextApiRequest, res: NextApiResponse) {
 
   const toHostMailData = {
     from: "aranea0395@gmail.com",
-    to: "apologus0395@gmail.com",
+    to: "ohmarket22@naver.com",
     subject: `${req.body.id}님이 수요조사를 완료하였습니다.`,
     text: `ID :${req.body.id} `,
     html: `
